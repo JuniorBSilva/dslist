ed07e30d2d694302b6dd9b0d6fc55002d27958a8
JuniorBSilva <junior.bsilva7@gmail.com>
2023-05-19 07:32:23 -0300

    endpoint para mover posição do game na lista

---
+++ src/main/java/com/devsuperior/dslist/dto/ReposicionamentoListDTO.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dslist.dto;
+
+public class ReposicionamentoListDTO {
+
+	private Integer indexOrigem;
+	private Integer indexDestino;
+	public Integer getIndexOrigem() {
+		return indexOrigem;
+	}
+	public void setIndexOrigem(Integer indexOrigem) {
+		this.indexOrigem = indexOrigem;
+	}
+	public Integer getIndexDestino() {
+		return indexDestino;
+	}
+	public void setIndexDestino(Integer indexDestino) {
+		this.indexDestino = indexDestino;
+	}
+	
+}
--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,10 +18,31 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 		return dto;
 	}
+	
+	@Transactional
+	public void mover(Long listId, int indexOrigem, int indexDestino) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(indexOrigem);
+		list.add(indexDestino, obj);
+		
+		int menorPosicao = indexOrigem < indexDestino ? indexOrigem : indexDestino;
+		int maiorPosicao = indexOrigem < indexDestino ? indexDestino : indexOrigem;
+		
+		for(int i = menorPosicao; i <= maiorPosicao; i++) {
+			gameListRepository.atualizaPosicao(listId, list.get(i).getId(), i);
+		}
+		
+	}
+	
 }
